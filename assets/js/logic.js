69866802532660c9d9c3fc2e0ff709c357d7344d
Glutey <mr_tscreen@hotmail.com>
2023-01-24 15:24:09 +0000

    Added SFX, functions and lots of comments.

---
+++ assets/js/logic.js
@@ -0,0 +1,57 @@
+//  To begin task we must declare all variables first;  so make a list (hints are left within the HTML for reference) 
+
+var time = 60;
+var timer;
+var currentQuestion = 0;
+var score = 0;
+var submit = ;
+// sound effcts for riggt and wrong answer
+var sfxRight = new Audio("assets/sfx/correct.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+                
+ // start game with an event listener - set up function to start timer and display questions.
+ var startQuiz = document.addEventListener("click"); // wrong, needs to be get ElementByID first, as that declares what is required
+ document.getElementById("start").addEventListener("click"); // this should then be startQuiz.addEventListener("click"); this then states what we want from it
+ 
+ const questionArray = questions; 
+ // we have access to the question.js due to the shared space in the index.HTML file. Thus allowing me to refer to variables declared in the question.js file.
+
+function startGame(){
+    document.getElementById("start")
+    function time(){
+    var timeLeft = 60;
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timer = setInterval(function () { 
+        // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        time.textContent = timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else (timeLeft === 0) {
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeLeft);
+      }; 1000})
+    }}
+
+
+  displayQuestion = function (){} // wrong... a statement should be declared first - then the name of the 
+
+  function displayQuestion() {
+      for (var i = 0; i < questions.length; i++)
+      if (currentQuestion < questions[i]) {
+        // lost...
+      }
+    }
+
+// function code for answer selection 
+function checkAnswer() {
+    var correct1 = "Domain Object Model";
+    var selected = document.querySelector('input[name="Domain Object Model"]:checked').value;
+    if (selected === correct1) {
+      sfxRight.play(); // calls the sound effect
+    }else {
+        sfxWrong.play();} // calls the sound effect
+
+
+
